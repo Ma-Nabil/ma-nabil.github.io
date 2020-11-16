43159b2221da60973693e49d8f1aa33d8b4af53c
Ma-Nabil <65572073+Ma-Nabil@users.noreply.github.com>
2020-11-16 16:56:27 +0200

    Update reg.js

--- js/reg.js
+++ js/reg.js
@@ -1,5 +1,6 @@
 /*global $*/
-var next;
+var next,
+	inputs;
 $("#register-form input").wrap("<div class='form-group'></div>");
 $("input:not([type='submit'])").addClass("form-control");
 $(".form-control").parent().prev().each(
@@ -14,36 +15,36 @@ $("input:not([type='submit'])").attr("data-toggle", "tooltip");
 $('[data-toggle="tooltip"]').tooltip({
 	placement: "left"
 });
-$("input[type='submit']").attr("data-toggle","popover");
-$("#register-form input:not([type='submit'])").on("blur",function(){toValidate($(this))});
-$("#register-form").on("submit",function(){
-	let inputs = $(this).find("input:not([type='submit'])");
+$("input[type='submit']").attr("data-toggle", "popover");
+$("#register-form input:not([type='submit'])").on("blur", function () { toValidate($(this)); });
+$("#register-form").on("submit", function () {
+	inputs = $(this).find("input:not([type='submit'])");
 	toValidate(inputs);
 });
 function toValidate(el) {
-	console.log(el);
 	if(!validate(el)) {
-		el.css("border","2px solid #f7696a");
+		el.css("border", "2px solid #f7696a");
 		el.tooltip('show');
 	} else {
-		el.css("border","1px solid #D7DCE0");
+		el.css("border", "1px solid #D7DCE0");
 		el.tooltip('hide');
 	}
 }
 function validate(el) {
-	console.log(el.attr("name"));
-	if(el.attr("name") == "username") {
-		if(el.val().trim().match(/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[أ-يa-zA-Z0-9._]+(?<![_.])$/) == null)
+	if (el.attr("name") == "username") {
+		if (el.val().trim().match(/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[أ-يa-zA-Z0-9._]+(?<![_.])$/) == null) {
 			return false;
-		} else if(el.attr("name") == "email") {
-			if(el.val().trim().match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/) == null)
-			return false;
-		} else if(el.attr("name") == "password") {
-			if(el.val().trim().length < 8 ) {
-				return false;
-			}
-		}  else if(el.has("required").val() == "") {
-		return false;
 		}
+	} else if(el.attr("name") == "email") {
+		if (el.val().trim().match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/) == null) {
+			return false;
+		}
+	} else if(el.attr("name") == "password") {
+		if (el.val().trim().length < 8 ) {
+			return false;
+		}
+	}  else if (el.has("required").val() == "") {
+		return false;
+	}
 	return true;
 }
